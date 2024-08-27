9194d4790852e5c94b048a4d464db11eafb3067b
10-5\mit <kuc00623@gmail.com>
2024-08-27 16:02:28 +0900

    V 2.0.4 (보드 페이징, 검색)

--- src/main/java/org/zerock/mapper/BoardMapper.java
+++ src/main/java/org/zerock/mapper/BoardMapper.java
@@ -2,26 +2,24 @@ package org.zerock.mapper;
 
 import java.util.List;
 
-
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 
 public interface BoardMapper {
+
 	public List<BoardVO> getList();
-	
-	// 객체 삽입
-	public void insert(BoardVO boardVO);
-	
-	// 번호를 미리 만들고 객체 삽입.
-	public void insertSelectKey(BoardVO boardVO);
-	
-	// 번호를 이용해서 객체 불러오기
+
+	public List<BoardVO> getListWithPaging(Criteria cri);
+
+	public void insert(BoardVO board);
+
+	public Integer insertSelectKey(BoardVO board);
+
 	public BoardVO read(Long bno);
-	
-	// 객체 수정하기
-	public int update(BoardVO boardVO);
-	
-	// 객체 삭제하기
+
 	public int delete(Long bno);
-	
-	
+
+	public int update(BoardVO board);
+
+	public int getTotalCount(Criteria cri);
 }
--- src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -1,17 +1,23 @@
 package org.zerock.mapper;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 
 import lombok.Setter;
+import lombok.extern.log4j.Log4j;
 import lombok.extern.log4j.Log4j2;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+// Java Config
+// @ContextConfiguration(classes = {org.zerock.config.RootConfig.class} )
 @Log4j2
 public class BoardMapperTests {
 
@@ -20,56 +26,95 @@ public class BoardMapperTests {
 
 	@Test
 	public void testGetList() {
-		mapper.getList().forEach(boardVO -> log.info(boardVO));
+
+		mapper.getList().forEach(board -> log.info(board));
+
 	}
 
 	@Test
 	public void testInsert() {
-		BoardVO boardVO = new BoardVO();
-		boardVO.setTitle("매퍼로 만든 제목");
-		boardVO.setContent("매퍼로 만든 내용");
-		boardVO.setWriter("매퍼 사용자");
-		mapper.insert(boardVO);
 
-		log.info(boardVO);
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글");
+		board.setContent("새로 작성하는 내용");
+		board.setWriter("newbie");
+
+		mapper.insert(board);
+
+		log.info(board);
 	}
 
 	@Test
 	public void testInsertSelectKey() {
-		BoardVO boardVO = new BoardVO();
-		boardVO.setTitle("셀렉트 키 제목");
-		boardVO.setContent("셀렉트 키 내용");
-		boardVO.setWriter("셀렉트 키");
-		mapper.insertSelectKey(boardVO);
 
-		log.info(boardVO);
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글 select key");
+		board.setContent("새로 작성하는 내용 select key");
+		board.setWriter("newbie");
+
+		mapper.insertSelectKey(board);
+
+		log.info(board);
 	}
 
 	@Test
 	public void testRead() {
-		BoardVO boardVO = mapper.read(5L);
 
-		log.info(boardVO);
+		// 존재하는 게시물 번호로 테스트
+		BoardVO board = mapper.read(5L);
+
+		log.info(board);
+
 	}
-	
-	@Test
-	public void testUpdate() {
-		BoardVO boardVO = new BoardVO();
-		boardVO.setBno(3L);
-		boardVO.setTitle("수정된 제목");
-		boardVO.setContent("수정된 내용");
-		boardVO.setWriter("수정한 사람");
-		
-		mapper.update(boardVO);
-		
-		log.info(boardVO);
-	}
-	
+
 	@Test
 	public void testDelete() {
-		
-		
-		log.info("삭제된 객체 수  : " + mapper.delete(4L));
+
+		log.info("DELETE COUNT: " + mapper.delete(3L));
 	}
+
+	@Test
+	public void testUpdate() {
+
+		BoardVO board = new BoardVO();
+		// 실행전 존재하는 번호인지 확인할 것
+		board.setBno(5L);
+		board.setTitle("수정된 제목");
+		board.setContent("수정된 내용");
+		board.setWriter("user00");
+
+		int count = mapper.update(board);
+		log.info("UPDATE COUNT: " + count);
+
+	}
+
+	@Test
+	public void testPaging() {
+
+		Criteria cri = new Criteria();
+		
+	    //10개씩 3페이지 
+	    cri.setPageNum(3);
+	    cri.setAmount(10);
+
+
+		List<BoardVO> list = mapper.getListWithPaging(cri);
+
+		list.forEach(board -> log.info(board));
+
+	}
+	
+	  @Test
+	  public void testSearch() {
+
+	    Criteria cri = new Criteria();
+	    cri.setKeyword("키워드");
+	    cri.setType("TCW");
+
+	    List<BoardVO> list = mapper.getListWithPaging(cri);
+
+	    list.forEach(board -> log.info(board));
+	  }
+
 
 }
